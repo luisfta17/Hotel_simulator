d34713ea2eedb7308dad38845fb83057b8710606
Luis Tejero <luisfaridta@gmail.com>
2018-05-18 14:23:08 +0100

    dinning room added

--- src/main/java/hotel/rooms/offeredRooms/DinningRoom.java
+++ src/main/java/hotel/rooms/offeredRooms/DinningRoom.java
@@ -5,9 +5,15 @@ import hotel.rooms.Room;
 public class DinningRoom  extends Room{
 
     private int capacity;
+    private String name;
 
-    public DinningRoom(int capacity){
+    public DinningRoom(int capacity, String name){
         super(capacity);
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
     }
 
 }
--- src/test/java/DinningRoomTest.java
+++ src/test/java/DinningRoomTest.java
@@ -1,2 +1,28 @@
+import hotel.rooms.offeredRooms.DinningRoom;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DinningRoomTest {
+    DinningRoom dinningRoom;
+
+    @Before
+    public void before(){
+        dinningRoom = new DinningRoom(100, "Ocaso");
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(100, dinningRoom.getCapacity());
+    }
+    @Test
+    public void hasName(){
+        assertEquals("Ocaso", dinningRoom.getName());
+    }
+    @Test
+    public void hasGuests(){
+        assertEquals(0, dinningRoom.getGuests().size());
+    }
+
 }
